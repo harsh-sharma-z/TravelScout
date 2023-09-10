ddc2d00c3f583ebed4dad023afb1d6e7c7b6d263
harsh-sharma-z <harsh.ramakant.sharma@gmail.com>
2023-09-10 14:00:23 +0530

    basic styling done

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -5,6 +5,7 @@ const map = new mapboxgl.Map({
     center: campground.geometry.coordinates , // starting position [lng, lat]
     zoom: 8, // starting zoom
 });
+map.addControl(new mapboxgl.NavigationControl()); 
 
 new mapboxgl.Marker({
     color: "#E60000",
